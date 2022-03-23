294fbda2b29cbde1d829535f38c8c62448918191
onumengine <onumdev@gmail.com>
2022-03-23 23:39:49 +0100

    create the library viewmodel and state

---
+++ lib/features/audio_query/presentation/library_viewmodel.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:player_one/core/utils/params.dart';
+import 'package:player_one/features/audio_query/domain/entities/track.dart';
+import 'package:player_one/features/audio_query/domain/use_cases/get_tracks.dart';
+
+class LibraryViewModel extends ChangeNotifier {
+  final GetTracks getTracks;
+
+  List<TrackEntity> _tracks = [];
+  List<TrackEntity> get tracks => _tracks;
+
+  LibraryState _state = LibraryState.unfetched;
+  LibraryState get state => _state;
+
+  LibraryViewModel({required this.getTracks}) {
+    refreshLibrary();
+  }
+
+  refreshLibrary() async {
+    final result = await getTracks(NoParams());
+    result.fold((failure) {
+      print(failure.message);
+    }, (tracks) {
+      _tracks = tracks;
+      notifyListeners();
+    });
+  }
+}
+
+enum LibraryState {
+  unfetched,
+  fetching,
+  fetched,
+  unableToPopulate,
+}
